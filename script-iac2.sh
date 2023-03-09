787dbe4c0a454cb81799582472e53e163c5f97a4
madson7 <madson.adm7@gmail.com>
2023-03-09 18:50:58 -0300

    Infraestrutura como Código - Script de Provisionamento de um Servidor Web

--- script-iac2.sh
+++ script-iac2.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-echo "Atualizando o servidor..."
-apt-get update
-apt-get upgrade -y
-apt-get install apache2 -y
-apt-get install unzip -y
+## Script deve ser executado com o usuário root e não usando sudo, dependência do cron!
 
+echo "Atualizando e instalando as dependencias no servidor..."
+apt-get update && \
+    apt-get upgrade -y && \
+    apt-get install -y apache2 unzip git
+
+clear
 
 echo "Baixando e copiando os arquivos da aplicação..."
+rm -rf /var/www/html
+git clone https://github.com/denilsonbonatti/linux-site-dio.git /var/www/html/
 
-cd /tmp
-wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
-unzip main.zip
-cd linux-site-dio-main
-cp -R * /var/www/html/
+systemctl --now apache2
 
+echo "Automatizando a atualização!"
 
-
+echo "0 * * * * root git pull /var/www/html/ >/dev/null 2>&1" >> /etc/crontab
+echo "0 * * * * root systemctl reload apache2 >/dev/null 2>&1" >> /etc/crontab
